bc4806ffd9b34f6701aada76b0126a9c644f6e0e
SoftNation <emmanuelsoft8105@gmail.com>
2024-12-17 22:50:38 +0100

    started the homepage

--- scripts/registerlogin.js
+++ scripts/registerlogin.js
@@ -1,50 +1,12 @@
-console.log('working')
-
-function generateRegisterHtml (){
-    let registerloginHtml = '';
-    registerloginHtml = `  <div class="fisrt-div">
-            <div class="first">
-                <h2>Creat Account</h2>
-                <input class="input" type="text" placeholder="Name">
-                <input class="input" type="text" placeholder="Email">
-                <input class="input" type="number" placeholder="Phone number">
-               <div class="button-div">
-                  <button>Creat Account</button>
-               </div>
-            </div>
-            <div class="second">
-                <h2>Hello Friend!</h2>
-                <p>Enter your perssonal details to start you journey with us</p>
-                <div class="button-div"><button id="sign">Sign In</button></div>
-            </div>
-        </div>  `;
-
-};
-
-function generateloginHtml (){
-    let loginHtml = '';
-    loginHtml = `    <div class="second-div"> 
-                <div class="first">
-                    <h2>Sign In</h2>
-                    <input class="input" type="text" placeholder="Email">
-                    <input class="input" type="number" placeholder="Phone number">
-                   
-                    <div class="button-div">
-                    <a href="">Forgot your password?</a>
-                    <button>Log In</button>
-                    </div>
-        </div>
-        <div class="second">
-            <h2>Welcome Back!</h2>
-            <p>Kindly input your personal info to login</p>
-            <div class="button-div"><button id="sign">Sign Up</button></div>
-        </div>
-    </div>`;
-};
 
 const registerloginsec = document.querySelector('.js-registerlogin');
 const signIn = document.querySelector('.js-signinbut');
 const signUp = document.querySelector('.js-signupbut');
+
+document.querySelector('.jslogin-button').addEventListener('click', ()=>{
+    console.log('working')
+    window.location.href = 'homepage.html';
+})
 
 const createAccountSection = document.querySelector('.creatacc-div');
 const welcomeBackSection = document.querySelector('.signin-div');
