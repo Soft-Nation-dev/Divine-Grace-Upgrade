8fbeb9819324549be40c0f15707a2be7d47518ff
soft-nation-dev <emmanuelsoft8105@gmail.com>
2025-05-26 21:13:24 +0100

    try again

--- scripts/divinegracegeneral.js
+++ scripts/divinegracegeneral.js
@@ -1,3 +1,3 @@
 document.querySelector('.js-login-button').addEventListener('click', ()=>{
-    window.location.href = '/Divine-Grace-Upgrade/registerlogin';
+    window.location.href = 'registerlogin';
 });
